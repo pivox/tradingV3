2a6365eb5d1f367e98850cad0f03871f253b400c
Haythem MABROUK <haythem.mabrouk@freepro.com>
2025-10-08 15:06:11 +0200

    command get assets

--- symfony-app/src/Command/Bitmart/AssetsDetailCommand.php
+++ symfony-app/src/Command/Bitmart/AssetsDetailCommand.php
@@ -20,7 +20,32 @@ final class AssetsDetailCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $resp = $this->assetsService->getAssetsDetail();
+        //$resp = $this->assetsService->getAssetsDetail();
+        try {
+            $resp = $this->assetsService->getAssetsDetail();
+            $code = $resp['code'] ?? null;
+            if ($code !== 1000) {
+                print_r('Cron 1m: échec récupération assets-detail, on annule l\'envoi'.json_encode(['response' => $resp]));
+            }
+            $data = $resp['data'] ?? [];
+            $usdtAvailable = 0.0;
+            foreach ($data as $row) {
+                if (($row['currency'] ?? null) === 'USDT') {
+                    $usdtAvailable = (float)($row['available_balance'] ?? 0);
+                    break;
+                }
+            }
+            if ($usdtAvailable <= 50.0) {
+                print_r("notify: Cron 1m: balance USDT insuffisante ($usdtAvailable), on annule l'envoi".json_encode(['response' => $resp]));
+                die;
+            }
+            // Log succès contrôle
+            print_r(["response" => $resp]);
+
+        } catch (\Throwable $e) {
+
+            die($e->getMessage());
+        }
         $output->writeln(json_encode($resp, JSON_PRETTY_PRINT));
         return Command::SUCCESS;
     }
