61a261d78934852aa3fde5d1bd8d801b53e6ca4e
Haythem MABROUK <haythem.mabrouk@freepro.com>
2025-11-16 23:44:23 +0100

    feat(GetFalseCondition): implement new condition for evaluating 'get_false' state

---
+++ trading-app/src/Indicator/Condition/GetFalseCondition.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Indicator\Condition;
+
+
+use App\Indicator\Attribute\AsIndicatorCondition;
+use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+#[AsIndicatorCondition(timeframes: ['15m','5m','1m','1h','4h'], name: ExpectedRMultipleLtCondition::NAME)]
+#[AutoconfigureTag('app.indicator.condition')]
+#[AsTaggedItem(index: ExpectedRMultipleLtCondition::NAME)]
+class GetFalseCondition extends AbstractCondition
+{
+    const NAME = 'get_false';
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function evaluate(array $context): ConditionResult
+    {
+        return $this->result(
+            self::NAME,
+            (bool) ($context['get_false'] ?? false),
+            [],
+            $this->baseMeta($context),
+        );
+    }
+}
